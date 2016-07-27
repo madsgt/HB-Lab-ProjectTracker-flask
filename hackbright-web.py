026fc979c98e47fbf4016a578754890dcd08a089
Hackbright Student <info@hackbrightacademy.com>
2016-07-27 12:59:39 -0700

    GRade listing done

--- hackbright-web.py
+++ hackbright-web.py
@@ -13,11 +13,17 @@ def get_student():
     """Show information about a student."""
 
     github = request.args.get('github', 'jhacks')
+    title = request.args.get('title', 'None')
+
     first, last, github = hackbright.get_student_by_github(github)
+    rows = hackbright.get_grades_by_title(title) #request arg for the title
+    print "\n\n\n\n\n WHAT IS THIS PRINTING\n\n\n", rows
     html = render_template("student_info.html", 
                             first=first, 
                             last=last,
-                            github=github)
+                            github=github,
+                            rows=rows
+                            )
     return html
 
 @app.route("/student_search")
@@ -27,8 +33,9 @@ def get_student_form():
 
     return render_template("student_search.html")
 
-@app.route("/student_addform")
+@app.route("/student_addform") 
 def student_addform():
+    """ Displays the form to capture the student details """
     return render_template("student_add.html")
 
 
@@ -40,7 +47,7 @@ def student_add():
     last = request.form.get("last_name")
     github = request.form.get("github")
 
-    hackbright.make_new_student(first, last, github)
+    hackbright.make_new_student(first, last, github) #this is adding it to the db
 
     flash("Successfully added Student")
 
